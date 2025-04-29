bfc9aad94c169ad4a1d2606977cf627813070b05
alexandrefnas <alexandrefnas@gmail.com>
2025-04-29 18:47:19 -0300

    refatorando codigo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import  {datafake}  from "src/app/data/datafake";
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title: string = ""
+  id: number = 0
+  photo:any
+  index = 0
+
+  question: any
+  busca:boolean = false
+
 
   constructor() { }
 
   ngOnInit(): void {
+      this.question = datafake
   }
 
 }
