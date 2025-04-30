073396e9a3fa672789191679073e931392165730
alexandrefnas <alexandrefnas@gmail.com>
2025-04-29 23:33:30 -0300

    Deploy GitHub via pasta docs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,14 +8,8 @@ import  {datafake}  from "src/app/data/datafake";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title: string = ""
-  id: number = 0
-  photo:any
-  index = 0
 
   question: any
-  busca:boolean = false
-
 
   constructor() { }
 
